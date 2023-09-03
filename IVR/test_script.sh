bf6d47a0d5bc9e1b1c74f09ec827ffbe2cfc3203
philo <csdp1122@csd.uoc.gr>
2023-09-04 01:58:20 +0300

    main

---
+++ IVR/test_script.sh
@@ -0,0 +1,30 @@
+
+cuda_device=0
+
+
+
+
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/osdd --auc  &> "results_osdd.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/osdd_cw --auc  &>> "results_osdd.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/osdd_ow --auc &>> "results_osdd.txt"   
+
+
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/cgqa_obj --auc  &> "results_cgqa.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/cgqa_cw --auc  &>> "results_cgqa.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/cgqa_ow --auc &>> "results_cgqa.txt"   
+
+
+
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/mit_obj --auc  &> "results_mit.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/mit_cw --auc  &>> "results_mit.txt"   
+
+CUDA_VISIBLE_DEVICES=$cuda_device python test.py --logpath logs/IVR/mit_ow --auc &>> "results_mit.txt"   
+
