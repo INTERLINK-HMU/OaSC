db36b91f7ce6f5a0561a160d235da527ed851e00
philo <csdp1122@csd.uoc.gr>
2023-09-04 22:59:24 +0300

    main commit

---
+++ SCEN/download_logs.sh
@@ -0,0 +1,31 @@
+
+
+mkdir logs
+
+
+
+
+
+#https://drive.google.com/file/d/1s7Npvw5K6I8WfnVzCeDbaDqRYh4C5NWG/view?usp=sharing
+
+
+fileid="1s7Npvw5K6I8WfnVzCeDbaDqRYh4C5NWG"
+filename="SCEN_logs.tar.gz"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o ${filename}
+
+
+tar -zxvf SCEN_logs.tar.gz -C logs/
+mv logs/*/* logs/
+
+rm -r logs/SCEN_best_logs
+rm cookie
+rm SCEN_logs.tar.gz 
+
+
+
+
+
+
+
+
